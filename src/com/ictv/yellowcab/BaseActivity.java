b7819cd71b3a22226f58017f0ccab13dbdb19dc5
Glen Aries C. Viernes <glenaries03@gmail.com>
2013-11-05 16:06:06 +0800

    master: test commit

--- src/com/ictv/yellowcab/BaseActivity.java
+++ src/com/ictv/yellowcab/BaseActivity.java
@@ -24,7 +24,7 @@ public class BaseActivity extends SlidingFragmentActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		//test
 		setTitle(mTitleRes);
 
 		// set the Behind View
